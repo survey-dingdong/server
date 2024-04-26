aa7b0e2fc92feccb13a2320198fc2d4e8a65f925
JongPil Yoon <whdvlf94@gmail.com>
2024-04-27 00:54:52 +0900

    Update pytest env

--- docker/init.sql
+++ docker/init.sql
@@ -1,7 +1,2 @@
-CREATE DATABASE survey_dingdong;
-GRANT ALL PRIVILEGES ON admin.* TO survey_dingdong@'%' WITH GRANT OPTION;
-
----
-
 CREATE DATABASE survey_dingdong_test;
-GRANT ALL PRIVILEGES ON admin.* TO survey_dingdong_test@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON survey_dingdong_test.* TO admin@'%' WITH GRANT OPTION;
